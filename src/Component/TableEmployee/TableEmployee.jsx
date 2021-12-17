2d34ab24221c91262e5275c565ff6fcf8d0b4397
Venik <kbalibrea@gmail.com>
2021-12-17 03:02:14 +0100

    table

--- src/Component/TableEmployee/TableEmployee.jsx
+++ src/Component/TableEmployee/TableEmployee.jsx
@@ -6,12 +6,14 @@ import { Input } from 'antd'
 export default function TableEmployee() {
     const employees = JSON.parse(localStorage.getItem('data'))
     const [dataSearched, setDataSearched] = useState(employees)
+    const [page, setPage] = useState()
+    const [pageSize, setPageSize] = useState()
     const itemRender = (current, type, originalElement) => {
         if (type === 'prev') {
-            return <a href="false">Previous</a>
+            return <button>Previous</button>
         }
         if (type === 'next') {
-            return <a href="false">Next</a>
+            return <button>Next</button>
         }
         return originalElement
     }
@@ -29,7 +31,15 @@ export default function TableEmployee() {
                         (e) =>{
                             const currValue= e.target.value
                             const filtered = employees.filter(entry =>
-                                entry.firstName.toLowerCase().includes(currValue)
+                                entry.firstName.toLowerCase().includes(currValue) ||
+                                entry.lastName.toLowerCase().includes(currValue) ||
+                                entry.startDate.toLowerCase().includes(currValue) ||
+                                entry.department.toLowerCase().includes(currValue) ||
+                                entry.dateOfBirth.toLowerCase().includes(currValue) ||
+                                entry.street.toLowerCase().includes(currValue) ||
+                                entry.city.toLowerCase().includes(currValue) ||
+                                entry.state.toLowerCase().includes(currValue) ||
+                                entry.zipCode.toLowerCase().includes(currValue)
                             )
                             setDataSearched(filtered)
                         }
@@ -40,15 +50,24 @@ export default function TableEmployee() {
                 columns={columns}
                 dataSource={dataSearched}
                 pagination={{
+                    current: page,
+                    pageSize: pageSize,
                     defaultPageSize: 10,
                     showSizeChanger: true,
                     pageSizeOptions: ['10', '25', '50', "100"],
                     showTotal:(total, range) =>{
                         return(
-                            <div> Showing {total} entries of {range} </div>
+                            <div> Showing {total} to {range} entries of {range} </div>
                         )
                     },
+                    onChange: (page, pageSize) =>{
+                        setPage(page)
+                        setPageSize(pageSize)
+                    },
                     itemRender:	itemRender
+                }}
+                rowKey={(range)=>{
+                    return range.firstName
                 }}
             />
         </React.Fragment>
